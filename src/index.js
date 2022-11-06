c9be4fc25968f36aabbbbf085621f675f44b82b6
sonnh <nguyenhongson.bk@gmail.com>
2022-11-06 14:10:10 +0700

    config react 17

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import './styles/global.scss';
+import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootNode  = document.getElementById('root');
+ReactDOM.render(<App />, rootNode);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
